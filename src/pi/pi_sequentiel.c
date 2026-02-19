8bee5eeddeb35690ef66f6ddbeecc7bac801550e
Antoine Pourthié <antoine.pourthie.etu@univ-lille.fr>
2026-02-19 21:08:32 +0100

    Correction de code : pi sequentiel

--- src/pi/pi_sequentiel.c
+++ src/pi/pi_sequentiel.c
@@ -1,45 +1,42 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-double pi_naif(long long N) {
-   double global_res = 0.0;
-    double invN = 1.0 / N;
-
-    #pragma omp parallel
-    {
-        int id = omp_get_thread_num();
-        int nb_threads = omp_get_num_threads();
-
-        long long intervalle = N / nb_threads;
-        long long debut = id * intervalle;
-        long long fin = (id == nb_threads - 1) ? N : (id + 1) * intervalle;
-
-        double local_res = 0.0;
-        for (long long i = debut; i < fin; i++) {
-            double x = (i + 0.5) * invN;
-            local_res += 4.0 / (x * x + 1.0);
-        }
-
-        #pragma omp critical
-        {
-            global_res += local_res;
-        }
-    }
-    return global_res * invN;
+double my_gettimeofday()
+{
+    struct timeval tmp_time;
+    gettimeofday(&tmp_time, NULL);
+    return tmp_time.tv_sec + (tmp_time.tv_usec * 1.0e-6L);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("USAGE: %s N\n", argv[0]);
-        return 1;
+double calculate_pi(long long N)
+{
+    double delta = 0.0;
+
+    for (int i = 0; i < N; i++)
+    {
+        double v = (double)i / N;
+        delta += 4 / (1 + v * v);
     }
+
+    return delta / N;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("USAGE: %s N\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     long long N = atoll(argv[1]);
 
-    double start = omp_get_wtime();
-    double result = pi_naif(N);
-    double end = omp_get_wtime();
+    double start = my_gettimeofday();
+    double result = calculate_pi(N);
+    double end = my_gettimeofday();
 
-    printf("Pi : %.10f | Temps : %lf s\n", result, end - start);
-    return 0;
+    printf("Pi : %.10f | Temps : %g s\n", result, end - start);
+    return EXIT_SUCCESS;
 }
